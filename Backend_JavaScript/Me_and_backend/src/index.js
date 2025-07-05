fea3d1adee4ede6c5fc8d62571cf82be8f7601a2
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-07-05 16:06:07 +0500

    added some middlewere in app.js

--- Backend_JavaScript/Me_and_backend/src/index.js
+++ Backend_JavaScript/Me_and_backend/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
@@ -7,7 +8,19 @@ dotenv.config({
 
 console.log(process.env.MONGODB_URI);
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("errotr", (error) => {
+      console.log("ERROR: ", error);
+      throw error;f
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running at port:", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed!!! ", error);
+  });
 
 /*
 import express from "express";
