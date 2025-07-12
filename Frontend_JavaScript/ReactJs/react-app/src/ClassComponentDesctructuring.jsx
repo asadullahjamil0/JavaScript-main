e1e254b8994c69c71d28108506c98a7790778b29
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-07-12 22:34:56 +0500

    Events, Event Handling in React.

--- Frontend_JavaScript/ReactJs/react-app/src/ClassComponentDesctructuring.jsx
+++ Frontend_JavaScript/ReactJs/react-app/src/ClassComponentDesctructuring.jsx
@@ -8,12 +8,28 @@ class ClassComponentDesctructuring extends Component {
       desc: "This is class component for destructuring in react",
     };
   }
+
+  changeContent = () => {
+    alert("Content changed in class component!");
+  };
+
   render() {
+    const helloName = (name) => {
+      alert("Hello " + name);
+    };
     const { model, desc } = this.state;
     return (
       <div>
         <h3>{model}</h3>
         <h3>{desc}</h3>
+        <button onClick={this.changeContent}>Click</button>
+        <button
+          onClick={() => {
+            helloName("Jamil");
+          }}
+        >
+          Print Name
+        </button>
       </div>
     );
   }
--- Frontend_JavaScript/ReactJs/react-app/src/DestructuringPropsAndState.jsx
+++ Frontend_JavaScript/ReactJs/react-app/src/DestructuringPropsAndState.jsx
@@ -1,10 +1,20 @@
 function DestructuringPropsAndState(props) {
+  const changeContent = () => {
+    alert("Content changed in function component!");
+  };
+
+  const helloName = (name) => {
+    alert("Hello " + name);
+  };
+
   const { name, age, city } = props;
   return (
     <>
       <h2>Name: {name}</h2>
       <h2>Age: {age}</h2>
       <h2>City: {city}</h2>
+      <button onClick={changeContent}>Click</button>
+      <button onClick={() => helloName("AsadUllah")}>Print Name</button>
     </>
   );
 }
