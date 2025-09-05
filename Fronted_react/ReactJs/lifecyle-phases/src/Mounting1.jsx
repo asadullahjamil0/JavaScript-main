c60e27980b03c1d6c34f0f3d6625e009354b0193
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-09-05 17:52:56 +0500

    Lifecycle phases: Mounting.

---
+++ Fronted_react/ReactJs/lifecyle-phases/src/Mounting1.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Mouting1 extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: 'ReactJS'
+        }
+        console.log("Mounting1 Constructore invoked!..");
+    }
+
+    render() {
+        console.log("Mounting1 Render invoked!..");
+        
+        return (
+            <div>
+                <h1>{this.state.name}</h1>
+                <h1>{this.props.city}</h1>
+            </div>
+        )
+    }
+}
