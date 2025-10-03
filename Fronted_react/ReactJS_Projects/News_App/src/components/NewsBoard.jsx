27d35251cc8a35f779c3ea25678748045e73fd8f
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-10-03 23:06:03 +0500

    Added some more bootstrap code for our news app project.

---
+++ Fronted_react/ReactJS_Projects/News_App/src/components/NewsBoard.jsx
@@ -0,0 +1,10 @@
+
+const NewsBoard = () => {
+  return (
+    <div>
+      <h1 className="text-center">Latest <span className="bg-danger badge">News</span></h1>
+    </div>
+  )
+}
+
+export default NewsBoard
