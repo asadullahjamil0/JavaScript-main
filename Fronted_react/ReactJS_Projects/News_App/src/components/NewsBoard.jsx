896433175285721b14eabe046e0e1e05c4b1f14f
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-10-13 14:34:48 +0500

    getting information from api for the latest news and display them on the website page.

--- Fronted_react/ReactJS_Projects/News_App/src/components/NewsBoard.jsx
+++ Fronted_react/ReactJS_Projects/News_App/src/components/NewsBoard.jsx
@@ -5,7 +5,7 @@ const NewsBoard = () => {
 
   const [articles, setArticles] = useState([])
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=c1f3ad9bd68440dfb3dea032ed1fcf57`
+    let url = `https://newsapi.org/v2/top-headlines?q=trump&apiKey=c1f3ad9bd68440dfb3dea032ed1fcf57`
     fetch(url)
       .then(response => response.json())
       .then(data => setArticles(data.articles))
@@ -13,12 +13,17 @@ const NewsBoard = () => {
 
   console.log(articles);
 
-
   return (
     <div>
       <h1 className="text-center">Latest <span className="bg-danger badge">News</span></h1>
       {articles.map((news, index) => {
-        <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} />
+        return <NewsItem
+          key={index}
+          title={news.title}
+          description={news.description}
+          src={news.urlToImage}
+          linkSrc={news.url}
+        />
       })}
     </div>
   )
