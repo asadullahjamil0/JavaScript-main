7851322c7c2d0ecd5335cb69207c519b4c077f8b
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-10-13 14:42:48 +0500

    Minor addition.

--- Fronted_react/ReactJS_Projects/News_App/src/components/NewsBoard.jsx
+++ Fronted_react/ReactJS_Projects/News_App/src/components/NewsBoard.jsx
@@ -2,20 +2,18 @@ import { useEffect, useState } from "react"
 import NewsItem from "./NewsItem"
 
 const NewsBoard = () => {
-
   const [articles, setArticles] = useState([])
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?q=trump&apiKey=c1f3ad9bd68440dfb3dea032ed1fcf57`
+    //getting information from NEWS Headlines API
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
     fetch(url)
       .then(response => response.json())
       .then(data => setArticles(data.articles))
   }, [])
-
-  console.log(articles);
-
   return (
     <div>
       <h1 className="text-center">Latest <span className="bg-danger badge">News</span></h1>
+      {/* Displaying data on the web page by using map function */}
       {articles.map((news, index) => {
         return <NewsItem
           key={index}
