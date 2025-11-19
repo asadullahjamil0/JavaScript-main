a1e90307e2dd8800af8b7bb6e9986915402854fa
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-11-19 21:53:30 +0500

    Interview questions for JavaScript and React. It's all about practise and preparation for interview.

---
+++ JavaScript_Projects/File1.js
@@ -0,0 +1,25 @@
+//Interview Question: Object into Object
+
+// const x = {name: "Asad", age: 24, city: "Sheikhupura"}
+// let b = {...x, name: "Jamil"}
+// const {name, ...xyz} = x
+// console.log(xyz, b);
+
+// Question 2: What will be the output if we accessing variable before its declaration for var, let and const.
+// console.log(pqr);
+// var pqr = 100;
+// console.log(pqr);
+
+// Question 3: Template literals
+// var x = "AsadUllah"
+// console.log(`${x}`);
+
+//question 4: What are the first order function and high order function.
+// question 5: Difference between Class components and Functional components.
+// question 6: what is the difference between useState() and useReducer().
+// question 7: What is the difference between Dev Dependency and Normal dependency.
+// Question 8: what is the difference between useMemo() and useCallback().
+// Question 9: what is the usage of keys and map method.
+// Question 10: What is Dynamic Routing.
+// Question 11: What is Actual DOM and Virtual DOM.
+// Question 12: Why React is fast than Angular or any other framework/library.
