513fdb5fab7ff8b2838d0ddb780239593b27576c
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-11-20 12:35:15 +0500

    Working on the NavBar in which i added some functionalities like toggler button according to the width of the page.

--- Fronted_react/ReactJS_Projects/bookSearch/src/Components/Navbar/Navbar.jsx
+++ Fronted_react/ReactJS_Projects/bookSearch/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Navbar.css"
+import logoImg from "../../images/logo.png"
+import { Link } from "react-router-dom"
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Navbar = () => {
+  const [ToggleMenu, setToggleMenu] = useState(false)
+  const handleNavBar = () => setToggleMenu(!ToggleMenu);
+
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className='navbar' id='navbar'>
+      <div className='container navbar-content flex'>
+        <div className='brand-and-toggler flex flex-sb'>
+          <Link to="/" className='navbar-brand flex'>
+            <img src={logoImg} alt="site logo" />
+            <span className='text-uppercase fw-7 fs-24 ls-1'>BookHub</span>
+          </Link>
+          <button type='button' className='navbar-toggler-btn' onClick={handleNavBar}>
+            <HiOutlineMenuAlt3 size={35} style={{ color: `${ToggleMenu ? "#fff" : "#010101"}` }} />
+          </button>
+        </div>
+      </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
